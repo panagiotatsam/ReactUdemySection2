2010ddc4826e295308ec98615e778b91bc743ac8
Panagiota Tsamasioti <77686758+panagiotatsam@users.noreply.github.com>
2024-08-26 14:07:07 +0300

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+//import { apiKey } from "./util";
+
+import apiKey from "./util";
+//import { apiKey, abc as content } from "./util";
+//import * as util from "./util";
+
+//console.log(util.abc);
+//console.log(content);
+console.log(apiKey);
